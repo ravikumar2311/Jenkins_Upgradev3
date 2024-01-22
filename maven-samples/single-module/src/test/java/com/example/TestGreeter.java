9c511055fcc269b88df9f502f97ef7d9b14e2878
ravikumar2311 <111011799+ravikumar2311@users.noreply.github.com>
2024-01-22 09:53:41 +0530

    Remove TestCases

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -28,11 +28,6 @@ public class TestGreeter {
   }
   
   @Test
-  public void FailureTestCase() {
-      assertTrue(true);
-  }
-  
-  @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
 
