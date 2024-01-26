e2adf7bbe95b2f74db6c53352a3b2309f094404c
ravikumar2311 <111011799+ravikumar2311@users.noreply.github.com>
2024-01-26 15:35:53 +0530

    Update Push.ps1

--- jenkins-plugin-model/ci/Push.ps1
+++ jenkins-plugin-model/ci/Push.ps1
@@ -1,8 +1,10 @@
 Param(
    $BuildNumber
 )
-docker image build -t ravikumar2311/jenkins-demo2:1 -f jenkins-plugin-model/src/demo2-publish/dockerfile .
+
+Write-Host "Build Number in Push Script : $($BuildNumber)"
+docker image build -t ravikumar2311/jenkins-demo2:$BuildNumber -f jenkins-plugin-model/src/demo2-publish/dockerfile .
 
 docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
 
-docker push ravikumar2311/jenkins-demo2:1
+docker push ravikumar2311/jenkins-demo2:$BuildNumber
