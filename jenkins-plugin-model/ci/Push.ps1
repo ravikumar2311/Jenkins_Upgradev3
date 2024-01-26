c24136094b72aaeb6d3b9c4b59689df3ee996466
ravikumar2311 <111011799+ravikumar2311@users.noreply.github.com>
2024-01-26 15:30:21 +0530

    Update Push.ps1

--- jenkins-plugin-model/ci/Push.ps1
+++ jenkins-plugin-model/ci/Push.ps1
@@ -1,7 +1,7 @@
 Param(
    $BuildNumber
 )
-docker image build -t ravikumar2311/jenkinsdotnetapplication:$BuildNumber .
+docker image build -t ravikumar2311/jenkins-demo2:$BuildNumber -f jenkins-plugin-model/src/demo2-publish/dockerfile .
 
 docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
 
