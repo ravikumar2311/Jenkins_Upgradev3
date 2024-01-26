4f7a26c7f7a80439d1c21afbd32a23a1da94587c
ravikumar2311 <111011799+ravikumar2311@users.noreply.github.com>
2024-01-26 13:59:48 +0530

    Create UnitTest.ps1

---
+++ jenkins-plugin-model/ci/UnitTest.ps1
@@ -0,0 +1,2 @@
+dotnet test --logger "trx;LogFileName=Pi.Math.trx" ./jenkins-plugin-model/src/Pi.Math.Tests/Pi.Math.Tests.csproj
+dotnet test --logger "trx;LogFileName=Pi.Runtime.trx" ./jenkins-plugin-model/src/Pi.Runtime.Tests/Pi.Runtime.Tests.csproj
